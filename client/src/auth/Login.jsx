85783f9934cd2e5a21c9ccba9a3f4aaef6fb74d0
manishkumar2654 <Hairbymanish@gmail.com>
2026-02-19 10:31:50 +0530

    manis9

--- client/src/auth/Login.jsx
+++ client/src/auth/Login.jsx
@@ -70,9 +70,9 @@ const Login = () => {
               <div className="w-full border-t border-gray-300" />
             </div>
             <div className="relative flex justify-center text-sm">
-              <span className="px-2 bg-white text-gray-500">
+              {/* <span className="px-2 bg-white text-gray-500">
                 Or continue with
-              </span>
+              </span> */}
             </div>
           </div>
 
--- client/src/auth/Register.jsx
+++ client/src/auth/Register.jsx
@@ -80,9 +80,9 @@ const Register = () => {
               <div className="w-full border-t border-gray-300" />
             </div>
             <div className="relative flex justify-center text-sm">
-              <span className="px-2 bg-white text-gray-500">
+              {/* <span className="px-2 bg-white text-gray-500">
                 Or create account with
-              </span>
+              </span> */}
             </div>
           </div>
 
