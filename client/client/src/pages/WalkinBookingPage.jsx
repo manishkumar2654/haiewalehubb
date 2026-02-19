8fe2d8cfe005940540e277a2d5206243881e8739
manishkumar2654 <Hairbymanish@gmail.com>
2026-02-19 11:35:46 +0530

    manis10

--- client/client/src/pages/WalkinBookingPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import WalkinBooking from "./walkin/WalkinBooking";
-
-const WalkinBookingPage = () => {
-  return <WalkinBooking className="pt-10" />;
-};
-
-export default WalkinBookingPage;
